88b2186fe3789d4976bc7cd0be9526cf24557e63
Marialer27 <maria.alerodriguez@gmail.com>
2024-10-19 20:14:51 -0300

    modificamos settings.py

--- back/Proyecto_Masc/masc/settings.py
+++ back/Proyecto_Masc/masc/settings.py
@@ -80,11 +80,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
   
 ]
