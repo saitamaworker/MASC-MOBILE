cbb5535071630b13afc51af7983cc7540a745913
lucas moyano <lucas.cba.382@gmail.com>
2024-10-22 22:53:52 -0300

    login funcionando

--- Masc_mobile/app/src/main/java/com/example/masc_mobile/LoginActivity.java
+++ Masc_mobile/app/src/main/java/com/example/masc_mobile/LoginActivity.java
@@ -1,12 +1,12 @@
 package com.example.masc_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.util.Log;
 
@@ -25,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText etEmail, etPassword;
     private Button btnLogin;
+    private TextView tvRegister;
     private RequestQueue rq;
 
     @Override
@@ -41,6 +42,15 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 login();
+            }
+        });
+        // Manejar clic en el TextView de registro
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirigir a la actividad de registro
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
             }
         });
     }
